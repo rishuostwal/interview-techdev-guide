c8fff14520aec3c3cf9fe01da18707044ad85055
Rishabh Ostwal <41901050+rishuostwal@users.noreply.github.com>
2019-10-26 22:20:07 +0530

    Update doublyLL.cpp

--- Data Structures/Doubly Linked List/CPP/doublyLL.cpp
+++ Data Structures/Doubly Linked List/CPP/doublyLL.cpp
@@ -9,17 +9,17 @@ class Node
 	Node* prev; 
 }; 
 
-void push(Node** head_ref, int new_data) 
+void push(Node* head_ref, int new_data) 
 { 
 	Node* new_node = new Node(); 
 	new_node->data = new_data; 
 
 	new_node->next = (*head_ref); 
 	new_node->prev = NULL; 
-	if ((*head_ref) != NULL) 
-		(*head_ref)->prev = new_node; 
+	if (head_ref != NULL) 
+		head_ref->prev = new_node; 
 
-	(*head_ref) = new_node; 
+	head_ref = new_node; 
 } 
 
 void insertAfter(Node* prev_node, int new_data) 
@@ -50,15 +50,15 @@ void append(Node** head_ref, int new_data)
 { 
 	Node* new_node = new Node(); 
 
-	Node* last = *head_ref; 
+	Node* last = head_ref; 
 
 	new_node->data = new_data; 
 
 	new_node->next = NULL; 
-	if (*head_ref == NULL) 
+	if (head_ref == NULL) 
 	{ 
 		new_node->prev = NULL; 
-		*head_ref = new_node; 
+		head_ref = new_node; 
 		return; 
 	} 
 	while (last->next != NULL) 
