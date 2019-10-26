2cf12d16b27c5ff396543a3f5b25d3cee5bd72dc
Rishabh Ostwal <41901050+rishuostwal@users.noreply.github.com>
2019-10-26 22:24:04 +0530

    Update Factorial.java

--- Algorithms/Maths/Factorial/Java/Factorial.java
+++ Algorithms/Maths/Factorial/Java/Factorial.java
@@ -1,26 +1,19 @@
-public class Factorial {
-  
-  static int factorialIterative(int num) {
-    if (num <= 1) return 1;
-
-    int total = 1;
-
-    for (int i = 1; i < num + 1; i++) {
-      total *= i;
-    }
-
-    return total;
-  }
-
-  static int factorialRecursive(int num) {
-    if (num <= 1) return 1;
-
-    return num * factorialRecursive(num - 1);
-  }
-
-  public static void main(String[] args) {
-    System.out.println(factorialIterative(5));
-    System.out.println(factorialRecursive(5));
-  }
-
-}
+class Test 
+{ 
+    // method to find factorial of given number 
+    static int factorial(int n) 
+    { 
+        if (n == 0) 
+          return 1; 
+          
+        return n*factorial(n-1); 
+    } 
+      
+    // Driver method 
+    public static void main(String[] args)  
+    { 
+        int num;
+        cin>>num;
+        System.out.println("Factorial of "+ num + " is " + factorial(5)); 
+    } 
+} 
